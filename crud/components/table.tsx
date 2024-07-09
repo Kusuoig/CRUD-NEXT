b51a42cfebb31acfe13156d3a3cef8d930b3a2d4
Kusuoig <diegovps134@gmail.com>
2024-07-08 23:41:58 -0600

    tabla01

---
+++ crud/components/table.tsx
@@ -0,0 +1,15 @@
+export default function Table (){
+return (
+
+ <table className="min-w-full table-auto ">
+    <thead>
+        <tr className="bg-slate-800">
+            <th className="px-16 py-2">
+                <span className="text-gray-400">Name</span>
+            </th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+ </table>
+);
+}
--- crud/src/app/page.tsx
+++ crud/src/app/page.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { FiPlusSquare } from "react-icons/fi";
+import Table from '../../components/table';
 
 export default function Home() {
   return (
     <section>
        <main className='py-5'> 
-      <h1 className='text-xl md:text-5xl text-center font-bold py-15'>MI CRUD</h1>
+      <h1 className='text-xl md:text-5xl text-center font-bold py-15'>CHUPAMELA</h1>
 
-      <div className='container mx-auto flex justify-between py-5 border-b'>
+      <div className='container mx-auto flex justify-between py-5  border-b'>
          <div className="left flex gap-3">
          <button className='flex bg-teal-600 px-4 py-2 text-white hover hover:bg-teal-700 border rounded-md hover:border-gray-500 hover:text-gray-300'>
           Agrega un producto <span className='px-1 flex-auto'><FiPlusSquare size={24}></FiPlusSquare></span> 
          </button>
          </div>
-
+          {/*form colapasble*/}
 
       </div>
+      {/*table*/}
+      <Table></Table>
     </main>
     </section>
    
